7e4f7938e3d24de3e4fa984752db76715782c51d
RamboYoung007700 <cqunanyang@hotmail.com>
2018-03-19 23:37:37 +0800

    DB
    DB DAO

---
+++ TestStuDAO.java
@@ -0,0 +1,18 @@
+package date;
+
+public class TestStuDAO {
+	public static void main(String[] args) {
+		Stu stu=new Stu("张三11",243243,"JAVA工程师","2018.3.20","家里蹲",
+				2343,"http://xxxxx.com","必胜必胜！","李四师兄","知乎看到");
+		StuDAO sd=new StuDAO();
+		sd.add(stu);
+		System.out.println(stu.id);
+		
+		stu.name="新的张三";
+		sd.update(stu);
+		System.out.println(stu);
+		sd.delete(3);
+		System.out.println(sd.GetFromName("张三11"));
+		System.out.println(sd.list());
+	}
+}
